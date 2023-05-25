5b5b19b90fde70c91b011e262d74d15cb7eaab63
onadan <onadan45@gmail.com>
2023-05-25 22:11:23 +0100

    new: axios and api folder feat:api call for signup and signin

---
+++ src/apis/index.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+export const BASE_URL = "https://laumga-membapp-api.onrender.com/";
+
+export async function signUp(firstName, lastName, email, password) {
+  return new Promise((resolve, reject) => {
+    axios
+      .post(`${BASE_URL}/auth/signup`, {
+        firstName,
+        lastName,
+        email,
+        password,
+      })
+      .then((response) => {
+        resolve(response.data);
+      })
+      .catch((err) => {
+        const errorMessage = err.response.data.error || err.message;
+        reject(new Error(errorMessage));
+      });
+  });
+}
+
+export async function signIn(email, password) {
+  return new Promise((resolve, reject) => {
+    axios
+      .post(`${BASE_URL}/auth/signin`, {
+        email,
+        password,
+      })
+      .then((response) => {
+        resolve(response.data);
+      })
+      .catch((err) => {
+        const errorMessage = err.response.data.error || err.message;
+        reject(new Error(errorMessage));
+      });
+  });
+}

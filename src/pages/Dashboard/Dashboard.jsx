2db82351846dd280e6977682c7df591c32ee9784
Osilaja78 <osilajaabdulhameed@gmail.com>
2023-05-30 08:22:56 +0100

    fixed mobile responsiveness for dashboard page

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -5,23 +5,25 @@ const Dashboard = () => {
   return (
     <section>
       <Navbar />
-      <div className="grid grid-cols-12 bg-hero-image min-h-screen bg-center bg-black/50 bg-blend-overlay font-mont">
-        <div className='col-span-3 h-full bg-[#f4f4f4] pt-28 mr-10'>
-          <div className='flex flex-col mb-10'>
-            <div className='mx-auto mb-4'>
-              <img src={President} alt="recent passport" className='rounded-full' />
+      <div className="md:grid md:grid-cols-12 bg-hero-image min-h-screen bg-center bg-black/50 bg-blend-overlay font-mont">
+        <div className='md:col-span-3 h-full bg-[#f4f4f4] pt-28 pb-5 md:pb-0 md:mr-10'>
+          <div className='flex justify-around md:block'>
+            <div className='flex flex-col ml-5 md:ml-0 md:mb-10'>
+              <div className='mx-auto mb-4'>
+                <img src={President} alt="recent passport" className='rounded-full' />
+              </div>
+              <div className='mx-auto'>Recent Passport</div>
             </div>
-            <div className='mx-auto'>Recent Passport</div>
-          </div>
-          <div className='flex flex-col'>
-            <div className='mx-auto mb-4 scale-90'>
-              <img src={President} alt="old passport" className='rounded-full' />
+            <div className='flex flex-col mr-3 md:mr-0'>
+              <div className='mx-auto mb-4 scale-90'>
+                <img src={President} alt="old passport" className='rounded-full' />
+              </div>
+              <div className='mx-auto'>Old Passport</div>
             </div>
-            <div className='mx-auto'>Old Passport</div>
           </div>
         </div>
         <div className='col-span-9'>
-        <div className="w-[90%] mx-auto mt-[150px] px-7 md:px-14 py-6 md:py-10">
+        <div className="w-[90%] mx-auto mt-[50px] md:mt-[150px] px-7 md:px-14 py-6 md:py-10">
           <div className="text-white text-center font-mont font-medium text-4xl">
             Personal Details
           </div>
@@ -39,7 +41,7 @@ const Dashboard = () => {
               <div>rahman.o@doshservices.com</div>
             </div>
             <div>
-              <div className='font-medium text-2xl'>Occupatin</div>
+              <div className='font-medium text-2xl'>Occupation</div>
               <div>Software Engineer</div>
             </div>
           </div>

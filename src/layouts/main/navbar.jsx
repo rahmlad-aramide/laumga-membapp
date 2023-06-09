a36d64c2f772a379cce4f505b76717feff07f209
Oladimeji Abdrahman Adebayo <abdrahmanoladimeji02@gmail.com>
2023-06-10 00:25:43 +0100

    update: added favicon

--- src/layouts/main/navbar.jsx
+++ src/layouts/main/navbar.jsx
@@ -17,8 +17,8 @@ export default function Navbar() {
           <Link to="/">
             <div className="flex items-center gap-3 sm:gap-4 md:gap-5 lg:gap-6 font-mont cursor-pointer">
               <img src={NewLogo} alt="" className="h-[48px] w-auto" />
-              <span className="font-semibold md:font-bold text-3xl sm:text-3xl md:text-4xl lg:text-[40px]">
-                MembApp
+              <span className="font-semibold md:font-bold text-3xl sm:text-3xl md:text-3xl lg:text-[35px]">
+                LAUMGA <br /> MembApp
               </span>
             </div>
           </Link>
